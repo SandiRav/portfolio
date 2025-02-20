72ff457f993763522c7740c02cd0a41b0f3a6d9d
SandiRav <harisoa.sn@gmail.com>
2025-02-20 19:43:57 +0300

    initial setup

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Home() {
+  return (
+    <div className="bg-red-200">
+      <main className="">
+        <h1>Portfolio</h1>
+      </main>
+      <footer className="">
+      </footer>
+    </div>
+  );
+}
