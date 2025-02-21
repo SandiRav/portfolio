78558e9f61019a884d66ad2ca9bd43563ce0c59e
SandiRav <harisoa.sn@gmail.com>
2025-02-21 21:26:30 +0300

    Integrate a font family and a few initial components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,5 @@
-
 export default function Home() {
-  return (
-    <div className="bg-red-200">
-      <main className="">
-        <h1>Portfolio</h1>
-      </main>
-      <footer className="">
-      </footer>
-    </div>
-  );
+  return <div className="font-sans">
+    <p>homepage</p>
+  </div>
 }
