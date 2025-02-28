d93deca208584586760c351b41b0533fdc73cee1
Arisoa <sandy.rav@onja.org>
2025-02-28 17:02:47 +0300

    Initial style for the nav

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,30 +1,63 @@
-import { IconMenu2 } from "@tabler/icons-react";
+"use client";
+
+import { IconMenu2, IconX } from "@tabler/icons-react";
+import Button from "./Button";
+import { useState } from "react";
 
 const Header = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function toggleMenu() {
+    console.log("showMenu::::::", showMenu);
+    setShowMenu(!showMenu);
+  }
+
   return (
-    <header className="container mt-5">
+    <header className="container mt-5 relative">
       <div className="lg:hidden flex justify-end">
-        <button className="button" aria-label="Icon Menu">
+        <Button ariaLabel="Menu" buttonType="button" onClick={toggleMenu}>
           <IconMenu2
-            aria-hidden={true}
             className="shrink-0 w-5 h-5 text-black"
+            aria-hidden={true}
           />
-        </button>
+        </Button>
       </div>
-      <nav className="absolute flex justify-center">
-        <ul className="flex lg:gap-x-32 gap-x-4">
+
+      <nav
+        className={`${
+          showMenu
+            ? "fixed inset-0 bg-opacity-80 flex justify-center items-center z-50"
+            : "hidden lg:flex justify-center absolute w-full"
+        }`}
+      >
+        <Button ariaLabel="Close" className="lg:hidden" buttonType="button" onClick={toggleMenu}>
+          <IconX className="shrink-0 w-6 h-6 text-black" aria-hidden={true} />
+        </Button>
+        <ul className="flex flex-col lg:flex-row items-center gap-y-6 lg:gap-x-32">
           <li>
-            <a href="/" className="text-primary-200">
+            <a
+              href="/"
+              className="text-white lg:text-primary-200 text-2xl lg:text-base"
+              onClick={() => setShowMenu(false)}
+            >
               Home
             </a>
           </li>
           <li>
-            <a href="/projects" className="text-primary-200">
+            <a
+              href="/projects"
+              className="text-white lg:text-primary-200 text-2xl lg:text-base"
+              onClick={() => setShowMenu(false)}
+            >
               Projects
             </a>
           </li>
           <li>
-            <a href="/get-in-touch" className="text-primary-200">
+            <a
+              href="/get-in-touch"
+              className="text-white lg:text-primary-200 text-2xl lg:text-base"
+              onClick={() => setShowMenu(false)}
+            >
               Get in touch
             </a>
           </li>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={inter.variable}>
-      <body>
+      <body className="bg-white">
         <Header />
         <main>{children}</main>
         <Footer />
